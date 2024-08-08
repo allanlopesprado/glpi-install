44e701edff73a1b76725cdc5ac7d1d14188cad26
Allan Lopes Prado <17826867+allanlopesprado@users.noreply.github.com>
2024-08-08 10:12:25 -0300

    Update glpi-install.sh

--- glpi-install.sh
+++ glpi-install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# -------------------------------------------------------------------------
+# @Name: glpi-install.sh
+# @Version: 1.0.0
+# @Date: 2024-08-08
+# @Author: Allan Lopes Prado
+# @License: GNU General Public License v2.0
+# @Description: Automates the installation of GLPI.
+# --------------------------------------------------------------------------
+# LICENSE
+#
+# glpi-install.sh is free software; you can redistribute and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# glpi-install.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software. If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
+
 # Function to display error messages
 error() {
   echo "Error: $1"
